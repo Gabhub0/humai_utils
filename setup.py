97d7567f71ef438a082b791e6203fc9f6cb0eab7
Gabriel Mansilla <gabriel001mansilla@gmail.com>
2023-10-01 19:27:37 -0300

    arreglando cagadas

--- setup.py
+++ setup.py
@@ -13,6 +13,6 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url='https://github.com/Gabhub0/humai_utils',
     license='MIT',
-    packages=['humai_utils'],
+    packages=['package'],
     install_requires=['requests'],
 )
