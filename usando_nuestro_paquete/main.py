5630f8f30ffa8bb025edddd395ba07f30c0e55b6
Gabriel Mansilla <gabriel001mansilla@gmail.com>
2023-10-01 19:55:17 -0300

    agregando nuevo pq

---
+++ usando_nuestro_paquete/main.py
@@ -0,0 +1,11 @@
+#importando el paquete completo
+import humai_utils
+
+#importando solo una funcion 
+
+from humai_utils import saludar_comunidad
+
+humai_utils.functions.informar_clase()
+
+saludar_comunidad()
+
